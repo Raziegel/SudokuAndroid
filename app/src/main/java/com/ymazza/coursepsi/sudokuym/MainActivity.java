2b5580db87c83e93b10182b9617dcd9ad5715100
Yoann <yoann.mazza1@gmail.com>
2018-04-25 08:14:58 +0200

    first commit

---
+++ app/src/main/java/com/ymazza/coursepsi/sudokuym/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ymazza.coursepsi.sudokuym;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
